6e9d3c2ef6842fba44aa9ea3c78cd5ac249b697d
Brian Gaffney <brian@deb.ug>
2020-12-18 17:16:37 +1100

    Install bridgecrew module for GCP project integration

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,12 +7,3 @@ provider "github" {
   token        = var.github_auth_token
   organization = "ONEOKI"
 }
-
-# To enable visibility in Bridgecrew, create a limited permission role
-# https://registry.terraform.io/modules/bridgecrewio/bridgecrew-gcp-read-only/google/latest
-# module "bridgecrew-read" {
-#   source                     = "bridgecrewio/bridgecrew-gcp-read-only/google"
-#   org_name               = "deb"
-#   bridgecrew_token = "872947f9-e3ba-55db-9bd7-5cfc0a13fa69"
-#   project_id = "github-stern"
-# }
